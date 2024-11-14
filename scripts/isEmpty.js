52b4811583790110e8d71130ebf7aef26b23279f
Danielevaccari <daniele.j.vaccari@gmail.com>
2024-11-14 20:27:29 +0200

    recopy script files

--- scripts/isEmpty.js
+++ scripts/isEmpty.js
@@ -1,12 +1,12 @@
-import getTag from "./.internal/getTag.js";
-import isArguments from "./isArguments.js";
-import isArrayLike from "./isArrayLike.js";
-import isBuffer from "./isBuffer.js";
-import isPrototype from "./.internal/isPrototype.js";
-import isTypedArray from "./isTypedArray.js";
+import getTag from './.internal/getTag.js'
+import isArguments from './isArguments.js'
+import isArrayLike from './isArrayLike.js'
+import isBuffer from './isBuffer.js'
+import isPrototype from './.internal/isPrototype.js'
+import isTypedArray from './isTypedArray.js'
 
 /** Used to check objects for own properties. */
-const hasOwnProperty = Object.prototype.hasOwnProperty;
+const hasOwnProperty = Object.prototype.hasOwnProperty
 
 /**
  * Checks if `value` is an empty object, collection, map, or set.
@@ -43,33 +43,27 @@ const hasOwnProperty = Object.prototype.hasOwnProperty;
  * // => false
  */
 function isEmpty(value) {
-	if (value == null) {
-		return true;
-	}
-	if (
-		isArrayLike(value) &&
-		(Array.isArray(value) ||
-			typeof value === "string" ||
-			typeof value.splice === "function" ||
-			isBuffer(value) ||
-			isTypedArray(value) ||
-			isArguments(value))
-	) {
-		return !value.length;
-	}
-	const tag = getTag(value);
-	if (tag == "[object Map]" || tag == "[object Set]") {
-		return !value.size;
-	}
-	if (isPrototype(value)) {
-		return !Object.keys(value).length;
-	}
-	for (const key in value) {
-		if (hasOwnProperty.call(value, key)) {
-			return false;
-		}
-	}
-	return true;
+  if (value == null) {
+    return true
+  }
+  if (isArrayLike(value) &&
+      (Array.isArray(value) || typeof value === 'string' || typeof value.splice === 'function' ||
+        isBuffer(value) || isTypedArray(value) || isArguments(value))) {
+    return !value.length
+  }
+  const tag = getTag(value)
+  if (tag == '[object Map]' || tag == '[object Set]') {
+    return !value.size
+  }
+  if (isPrototype(value)) {
+    return !Object.keys(value).length
+  }
+  for (const key in value) {
+    if (hasOwnProperty.call(value, key)) {
+      return false
+    }
+  }
+  return true
 }
 
-export default isEmpty;
+export default isEmpty
--- scripts/toString.js
+++ scripts/toString.js
@@ -1,12 +1,7 @@
-import isSymbol from "./isSymbol.js";
-
-/////////////////////////////////////////
-////// Daniele Vaccari 2024.10.20
-////// This file is not part of our tests but is included because our tests files call it as a dependency
-/////////////////////////////////////////
+import isSymbol from './isSymbol.js'
 
 /** Used as references for various `Number` constants. */
-const INFINITY = 1 / 0;
+const INFINITY = 1 / 0
 
 /**
  * Converts `value` to a string. An empty string is returned for `null`
@@ -28,21 +23,20 @@ const INFINITY = 1 / 0;
  * // => '1,2,3'
  */
 function toString(value) {
-	// Exit early for strings to avoid a performance hit in some environments.
-	if (typeof value === "string") {
-		return value;
-	}
-	if (Array.isArray(value)) {
-		// Recursively convert values (susceptible to call stack limits).
-		return `${value.map(other =>
-			other == null ? other : toString(other)
-		)}`;
-	}
-	if (isSymbol(value)) {
-		return value.toString();
-	}
-	const result = `${value}`;
-	return result == "0" && 1 / value == -INFINITY ? "-0" : result;
+  
+  // Exit early for strings to avoid a performance hit in some environments.
+  if (typeof value === 'string') {
+    return value
+  }
+  if (Array.isArray(value)) {
+    // Recursively convert values (susceptible to call stack limits).
+    return `${value.map((other) => other == null ? other : toString(other))}`
+  }
+  if (isSymbol(value)) {
+    return value.toString()
+  }
+  const result = `${value}`
+  return (result == '0' && (1 / value) == -INFINITY) ? '-0' : result
 }
 
-export default toString;
+export default toString
